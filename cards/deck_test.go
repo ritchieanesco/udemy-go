c9fc33d449e31784baa0570eeeed1c8bb2dd2f30
Ritchie Anesco <ritchie.anesco@anz.com>
2019-12-21 22:47:40 +1100

    Add more deck tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,7 +5,16 @@ import "testing"
 // t is the test handler
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
-	if len(d) != 16 {
-		t.Errorf("Expected deck length of 16, but got %d", len(d))
+	l := len(d)
+	if l != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", l)
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[l-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[l-1])
 	}
 }
